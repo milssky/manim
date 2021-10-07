a7a63685fe096203cf0603c8324ac0dddbd5da37
milssky <2furius@gmail.com>
2021-10-07 12:59:45 +0300

    SquateToStar scene finished

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 from manim import *
 
 
-class SquareToCircle(Scene):
+class SquareToStar(Scene):
     def construct(self):
         square = Square()
         square.set_fill(GREEN, opacity=0.5)
+        star = Star(outer_radius=2, color = BLUE)
         self.play(Create(square)) 
+        self.play(Transform(square, star))
+        self.play(FadeOut(square))
+
